421df60b3f0444a97187d3ccb6df719a98336281
Mario <123633800+mescu@users.noreply.github.com>
2024-07-13 17:11:47 +0200

    Update anonmeproxy.sh

--- anonmeproxy.sh
+++ anonmeproxy.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # Script created by mescu
 
 echo "https://twitter.com/mescu"
@@ -55,5 +55,57 @@ proxychains_verify() {
     fi
 }
 
+add_proxy() {
+    proxy_details=$1
+    echo "Do you want to add the proxy to the proxychains configuration? (y/n)"
+    read -r addproxy
+
+    if [[ "$addproxy" == "y" ]]; then
+        sudo sh -c "echo '$proxy_details' >> /etc/proxychains.conf"
+        if [ $? -eq 0 ]; then
+            echo "Proxy added to proxychains configuration successfully."
+        else
+            echo "Failed to add proxy to proxychains configuration."
+        fi
+    else
+        echo "Operation cancelled."
+    fi
+}
+
+check_proxy() {
+    echo
+    echo "Enter the proxy details (format: protocol://ip:port):"
+    read -r proxy_details
+
+    protocol=$(echo "$proxy_details" | awk -F '://' '{print $1}')
+    ip_port=$(echo "$proxy_details" | awk -F '://' '{print $2}')
+    ip=$(echo "$ip_port" | awk -F ':' '{print $1}')
+    port=$(echo "$ip_port" | awk -F ':' '{print $2}')
+
+    echo "Checking if the IP is accessible..."
+    if ping -c 2 "$ip" >/dev/null; then
+        echo "IP is accessible. Now adding the proxy to the configuration..."
+        add_proxy "$protocol $ip $port"
+    else
+        echo "The IP is not accessible."
+    fi
+}
+
+proxychains_logo() {
+    clear
+    echo -e "\e[32m    
+        _______            
+      /\       \           
+     /()\   ()  \          
+    /    \_______\         
+    \    /()     /         
+     \()/   ()  /          
+      \/_____()/
+  
+    AnonMeProxy 0.1\e[0m"
+}
+
 update_sys
 proxychains_verify
+proxychains_logo
+check_proxy
