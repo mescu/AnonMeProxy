c95d749bcac2f3a2d43538a08dee744d2114cdca
Mario <123633800+mescu@users.noreply.github.com>
2024-07-13 22:21:12 +0200

    Update anonmeproxy.sh

--- anonmeproxy.sh
+++ anonmeproxy.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+# !/bin/bash
 # Script created by mescu
-
-echo "https://twitter.com/mescu"
-echo "https://github.com/mescu"
-echo "AnonMeProxy 0.1"
-echo
 
 update_sys() {
     echo
@@ -12,7 +7,7 @@ update_sys() {
     echo "Do you want to proceed? (y/n)"
     read -r truesys
 
-    if [[ "$truesys" == "y" ]]; then
+    if [ "$truesys" == "y" ]; then
         echo "Updating and upgrading your system..."
         sudo apt-get update && sudo apt-get upgrade -y
         
@@ -38,7 +33,7 @@ proxychains_verify() {
         echo "Do you want to install it now? (y/n)"
         read -r trueinstallproxy
 
-        if [[ "$trueinstallproxy" == "y" ]]; then
+        if [ "$trueinstallproxy" == "y" ]; then
             echo "Installing proxychains..."
             sudo apt-get install -y proxychains
 
@@ -52,23 +47,6 @@ proxychains_verify() {
             echo "If you don't install proxychains, you can't continue!"
             exit 1
         fi
-    fi
-}
-
-add_proxy() {
-    proxy_details=$1
-    echo "Do you want to add the proxy to the proxychains configuration? (y/n)"
-    read -r addproxy
-
-    if [[ "$addproxy" == "y" ]]; then
-        sudo sh -c "echo '$proxy_details' >> /etc/proxychains.conf"
-        if [ $? -eq 0 ]; then
-            echo "Proxy added to proxychains configuration successfully."
-        else
-            echo "Failed to add proxy to proxychains configuration."
-        fi
-    else
-        echo "Operation cancelled."
     fi
 }
 
@@ -91,6 +69,55 @@ check_proxy() {
     fi
 }
 
+add_proxy() {
+    proxy_details=$1
+    echo "Do you want to add the proxy to the proxychains configuration? (y/n)"
+    read -r addproxy
+
+    if [ "$addproxy" == "y" ]; then
+        sudo sh -c "echo '$proxy_details' >> /etc/proxychains.conf"
+        if [ $? -eq 0 ]; then
+            echo "Proxy added to proxychains configuration successfully."
+        else
+            echo "Failed to add proxy to proxychains configuration."
+        fi
+    else
+        echo "Operation cancelled."
+    fi
+}
+
+proxy_ut() {
+    echo
+    echo "Select an option to use AnonMeProxy:"
+    echo "1) Open Firefox with AnonMeProxy"
+    echo "2) Use Nmap with AnonMeProxy"
+    echo "3) Run a custom command with AnonMeProxy"
+    read -r proxyut
+
+    case $proxyut in
+        1)
+            echo "Opening Firefox with AnonMeProxy..."
+            proxychains firefox www.google.com
+            ;;
+        2)
+            echo "Enter the Nmap command:"
+            read -r nmap_command
+            echo "Running Nmap with AnonMeProxy..."
+            proxychains nmap $nmap_command
+            ;;
+        3)
+            echo "Enter the custom command to run with AnonMeProxy:"
+            read -r custom_command
+            echo "Running custom command with AnonMeProxy..."
+            proxychains $custom_command
+            ;;
+        *)
+            echo "Invalid option. Exiting."
+            exit 1
+            ;;
+    esac
+}
+
 proxychains_logo() {
     clear
     echo -e "\e[32m    
@@ -103,9 +130,16 @@ proxychains_logo() {
       \/_____()/
   
     AnonMeProxy 0.1\e[0m"
+    
+    echo
+    echo "https://twitter.com/mescu"
+    echo "https://github.com/mescu"
+    echo "https://www.linkedin.com/in/mescu/"
+    echo
 }
 
 update_sys
-proxychains_verify
 proxychains_logo
+proxychains_verify
 check_proxy
+proxy_ut
